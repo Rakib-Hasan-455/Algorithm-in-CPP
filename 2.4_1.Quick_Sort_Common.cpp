4fb5f3e136a91a5acb3e4cf2314cedf6e3e2a8dd
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2021-08-18 14:03:29 -0700

    1.Common 2.Common Modified 3. Advanced Fast QuickSort

---
+++ 2.4_1.Quick_Sort_Common.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int partitionAC (int array[], int low, int high){
+	int pivot = array[high];    // pivot 
+    int i = (low - 1);
+   
+    for (int j = low; j <= high - 1; j++) 
+    { 
+        //if current element is smaller than pivot, increment the low element
+        //swap elements at i and j
+        if (array[j] < pivot) 
+        { 
+            i++;    // increment index of smaller element 
+            swap(array[i], array[j]); 
+        } 
+    } 
+    swap(array[i + 1], array[high]); 
+    return (i + 1); 
+}
+
+
+void quickSortAccend(int array[], int low, int high)
+{
+    if (low < high) 
+    { 
+        //partition the array 
+    int pivot = partitionAC(array, low, high); 
+       //sort the sub arrays independently 
+        quickSortAccend(array, low, pivot - 1); 
+        quickSortAccend(array, pivot + 1, high); 
+    } 
+}
+
+
+
+int main()
+{
+      int array[100005], n;
+      cin >> n;
+       for (int i = 0; i < n; i++)
+        {
+        	cin >> array[i];
+      	}
+      	clock_t sortStart = clock();
+	    quickSortAccend(array, 0, n-1);
+	    clock_t sortEnd = clock();
+    double sortTime = (double)(sortEnd-sortStart)/CLOCKS_PER_SEC;
+	  for (int i = 0; i < n; i++){
+        cout << array[i] << endl;
+    }
+    return 0;
+}
